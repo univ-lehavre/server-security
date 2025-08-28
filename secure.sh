58cae8c14f233d2d4d89ed458d5306cfa2990126
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-08-28 18:08:35 +0200

    Mise à jour de la commande ansible-playbook pour inclure les options --ask-pass et --ask-become-pass, ajout de commentaires explicatifs

--- secure.sh
+++ secure.sh
@@ -1,1 +1,3 @@
-set -a; source .env; set +a; ansible-playbook --hosts hosts.yml secure.yml
+set -a; source .env; set +a; ansible-playbook --ask-pass --ask-become-pass --inventory hosts.yml secure.yml
+# Une fois que la sécurité de base est en place, vous pouvez exécuter plutôt cette commande
+# set -a; source .env; set +a; ansible-playbook --inventory hosts.yml secure.yml
