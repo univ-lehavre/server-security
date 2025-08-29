bd4aab716346e547bd391062d7b9d4ead5eeaaa3
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-08-29 12:02:13 +0200

    Suppression du script secure.sh, les commandes sont désormais documentées ailleurs

--- secure.sh
+++
@@ -1,3 +0,0 @@
-set -a; source .env; set +a; ansible-playbook --ask-pass --ask-become-pass --inventory hosts.yml secure.yml
-# Une fois que la sécurité de base est en place, vous pouvez exécuter plutôt cette commande
-# set -a; source .env; set +a; ansible-playbook --inventory hosts.yml secure.yml
